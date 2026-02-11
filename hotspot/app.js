7144dbeedca96679a64cd7b644f6351c95190f54
victorkenyani <victorwakenyani@gmail.com>
2026-02-11 23:34:47 +0300

    Enable trust proxy and add /get-ip endpoint
    Added trust proxy setting and new endpoint to get client IP.

--- hotspot/app.js
+++ hotspot/app.js
@@ -6,12 +6,16 @@ const fs = require("fs")
 const server = require('http').Server(app)
 const port = process.env.PORT || 3000;
 
-
+app.set('trust proxy', true);
 app.use(express.static("./home"))
 app.use(bodyParser.json()) 
 app.get("/",(req, res)=>{
 	res.send("welcome to luche")
 })
+app.get('/get-ip', (req, res) => {
+  const clientIp = req.ip; // This will now hold the client's actual IP
+  res.json({ ip: clientIp });
+});
 app.post("/",(req, res)=>{
 	res.json({data:"rerrtr"})
 });
