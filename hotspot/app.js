f8bb63bd4ac0ecbb462a5df80d3d74500eb8f210
victorkenyani <victorwakenyani@gmail.com>
2026-02-14 01:54:40 +0300

    Update app.js

--- hotspot/app.js
+++ hotspot/app.js
@@ -1,33 +1,41 @@
-const express = require('express')
-const app = express()
-const reload = require('reload')
+const express = require('express');
 var bodyParser = require('body-parser')
-const fs = require("fs")
-const server = require('http').Server(app)
-const port = process.env.PORT || 3000;
+const reload = require('reload')
 
-app.set('trust proxy', true);
+const hostname = "localhost";
+const port = process.env.PORT || 3000;
+const app = express();
+const server = require('http').Server(app);
+
 app.use(express.static("./home"))
-app.use(bodyParser.json()) 
-app.get("/",(req, res)=>{
-	res.send("welcome to luche")
-})
+app.use(bodyParser.json());
+app.set('trust proxy', true); 
 app.get('/get-ip', (req, res) => {
   const clientIp = req.ip; // This will now hold the client's actual IP
   res.json({ ip: clientIp });
 });
-app.post("/",(req, res)=>{
-	res.json({data:"rerrtr"})
+app.get("/",(req, res)=>{
+    console.log(req.originalUrl)
+        
+        const clientIp = req.ip; // This will now hold the client's actual IP
+        console.log({ ip: clientIp });
+        res.send('trdytdy');
+
 });
-app.use("/request",require("./router/request"))
+
+app.post("/",(req, res)=>{
+        const body = req.body
+        res.json({token:"234567ertyu"});
+});
+
+app.use("/voucher", require("./router/voucher"))
 reload(app).then(function (reloadReturned) {
-	
-	}).catch(function (err) {
-	  console.error(err) 
-})
+  
+  }).catch(function (err) {
+    console.error(err) 
+});
 
-
-server.listen(port, () =>{
-	console.log(`http://localhost:${port}`)
-})
+server.listen(port,()=>{
+        console.log(`http://localhost:${port}`);
+});
 
